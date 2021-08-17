f4f00d5e8131487c0adc8cec9533ec69a2359fe5
Dawie <dvanlill@sun.ac.za>
2021-08-17 08:25:10 +0200

    Start with live coding session

---
+++ notebooks/dynamic/lecture0-julia.jl.jl
@@ -0,0 +1,48 @@
+### A Pluto.jl notebook ###
+# v0.15.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ aebb3152-91f5-4f21-90e3-7e4f97952ff8
+html"""
+<div style="
+position: absolute;
+width: calc(100% - 30px);
+border: 50vw solid #2e3440;
+border-top: 200px solid #2e3440;
+border-bottom: none;
+box-sizing: content-box;
+left: calc(-50vw + 15px);
+top: -100px;
+height: 100px;
+pointer-events: none;
+"></div>
+
+<div style="
+height: 200px;
+width: 100%;
+background: #2e3440;
+color: #fff;
+padding-top: 50px;
+">
+<span style="
+font-family: Vollkorn, serif;
+font-weight: 700;
+font-feature-settings: 'lnum', 'pnum';
+"> <p style="
+font-size: 1.5rem;
+opacity: 0.8;
+">ATS 872: Lecture 0</p>
+<p style="text-align: center; font-size: 1.8rem;">
+ Introduction to Julia (live coding)
+</p>
+
+<style>
+body {
+overflow-x: hidden;
+}
+</style>"""
+
+# ╔═╡ Cell order:
+# ╟─aebb3152-91f5-4f21-90e3-7e4f97952ff8
